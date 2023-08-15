46437f437c79b0c499612d21a6f2ba7bd210a61c
S1a0m <remveille@gmail.com>
2023-08-15 21:46:16 +0100

    le protptype de base du code

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+print("""
+ *********BIENVENUE**************************************************************************
+**********************************************************************************************
+                                          DDDDDDDDDD                                        **
+                                          DDDDDDDDDDD                                       **
+                                          DDDD    DDDD                                      **
+                                          DDDD     DDDD         SSSSSSSSSSSSSSSS            **
+       cccccccccccc      oooooooooooo     DDDD      DDDD       SSSSSSSSSSSSSSSSSS           **
+      ccccccccccccc     oooooooooooooo    DDDD       DDDD     SSSS                          **
+     cccc              oooo        oooo   DDDD        DDDD   SSSS                           **
+    cccc              oooo          oooo  DDDD         DDDD   SSSS                          **
+    cccc	          oooo          oooo  DDDD        DDDD     SSSSSSSSSSSSSSSSSS           **
+     cccc              oooo        oooo   DDDD       DDDD       SSSSSSSSSSSSSSSSSS          **
+      ccccccccccccc     oooooooooooooo    DDDDDDDDDDDDDD                      SSSSSS        **
+       cccccccccccc      oooooooooooo     DDDDDDDeoDDDD                        SSSSSS       **           
+                                                                              SSSSSSSS      **                        
+       sssssssssssssssssssssssssssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSS      **
+                            ssssssssssssssssssssssssssssssssssSamSSSSSSSSSSSSSSSSSSSS       **
+           ssssssssssssssssssssssssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSS        **
+               ssssssssssssssssssssssssssssssssssssssssssssssssssssssssSSSSSSSSSSSS         **
+                                                                                            **
+ Auteurs: AMOUSSOU P. Samson                                                                **
+          AMOUSSOU D. Sylvestre                                                             **
+<< Cryptons, non... Codons. Amusons nous à crypter*********************************** >>******
+**********************************************************************************************
+ ***************************************************************************Non! Décryptons!*
+                   """)
 
-# Press Shift+F10 to execute it or replace it with your
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("""Ce présent programme est juste une petite idée née lors d'une fantaisie entre son
+petit frère et l'auteur du code. Il s'agira à travers le programme de crypter du texte ou 
+de le décrypter en précisant le nom du fichier ou le chemin d'accès au fichier.
+
+***********
+*MENU: Cliquer
+a : Pour crypter
+b : Pour décrypter
+c : Pour passer root
+q : Pour quitter
+**********************
+***********************
+
+""")
+
+class Fichier_coDS:
+    def __init__(self, proprio, name_fichier, type_fichier, key):
+        self.proprio = proprio                                         # propriétaire du fichier
+        self.name_fichier = name_fichier
+        self.type_fihier = type_fichier
+        self._key = self._crypt_key(key)                                 # cle du fichier
+
+    def crypter(self):
+
+    def decrypter(self):
+
+    def info_fichier(self):
+
+    def _crypt_key(self):
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+opt_choisie = input("[coDS] >> ")                                      # option choisie dans le menu
+flag = True                                                            # pour continuer dans la boucle
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while flag:
+    if opt_choisie == 'a':
+        print('a')
+        break
+    elif opt_choisie == 'b':
+        print('b')
+        break
+    elif opt_choisie == 'c':
+        print('c')
+        break
+    elif opt_choisie == 'q':
+        print("À bientôt :-)")
+        flag = False
+    else:
+        print("""Vous n'avez pas saisie une option du menu.
+        Veuillez reprendre!""")
+        opt_choisie = input("[coDS] >> ")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
